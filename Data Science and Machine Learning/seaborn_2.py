3f27353c979c9abee3712f41cc6d8afee5fc395e
Hamed FirouziPouyaei <hamed.firouzipouyaei@vub.be>
2025-07-30 08:20:21 +0200

    Creating new branch for linux environment

--- Data Science and Machine Learning/seaborn_2.py
+++ Data Science and Machine Learning/seaborn_2.py
@@ -1,4 +1,22 @@
+import tkinter as tk
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('TkAgg') 
 tips = sns.load_dataset("tips")
+
+root = tk.Tk()
+root.title("Seaborn Visualization")
+
 flights = sns.load_dataset("flights")
+
+
+## Matrix Plot
+# For this to work, all data must be in a matrix format. meaning that the data must be in a 2D array-like structure.
+# using .corr() to get the correlation matrix
+
+# Only select numeric columns for correlation
+tc = tips.corr(numeric_only=True)
+print(tc)
+sns.heatmap(tc, annot=True, cmap='coolwarm')   
+plt.show()
